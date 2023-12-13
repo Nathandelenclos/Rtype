acb7c474cd6deab694a755ed84343bf1d12ba065
TalleuxEwen <ewen.talleux@epitech.eu>
2023-12-13 14:59:40 +0200

    feat: IGame interface class and now RType is a child of IGame

--- lib/r_type/RType.hpp
+++ lib/r_type/RType.hpp
@@ -5,9 +5,23 @@
 #ifndef BOOTSTRAP_RTYPE_HPP
 #define BOOTSTRAP_RTYPE_HPP
 
+#include <iostream>
+#include "../IGame.hpp"
 
-class RType {
 
+class RType : public IGame {
+public:
+    RType() {
+        std::cout << "RType constructor" << std::endl;
+    }
+
+    ~RType() override {
+        std::cout << "RType destructor" << std::endl;
+    }
+
+    void run() override {
+        std::cout << "RType::run()" << std::endl;
+    }
 };
 
 
