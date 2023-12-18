3db1f994807c9185b10b52ab269378bd21cff35d
TalleuxEwen <ewen.talleux@epitech.eu>
2023-12-18 17:09:33 +0200

    feat: class for server networking with init, send, receive and run methods (not tested yet because no client)

--- networking/server/ServerSocket.cpp
+++ networking/server/ServerSocket.cpp
@@ -1,5 +1,57 @@
-//
-// Created by talleux on 12/13/23.
-//
-
 #include "ServerSocket.hpp"
+#include <stdexcept>
+#include <iostream>
+
+ServerSocket::ServerSocket() {
+    sockfd = socket(AF_INET, SOCK_DGRAM, 0);
+    if (sockfd < 0) {
+        throw std::runtime_error("Failed to create socket");
+    }
+    std::cout << "Socket created successfully (fd: " << sockfd << ")" << std::endl;
+}
+
+ServerSocket::~ServerSocket() {
+    close(sockfd);
+}
+
+void ServerSocket::init_server(std::string ip, int port) {
+    memset(&serv_addr, 0, sizeof(serv_addr));
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_port = htons(port);
+    if (inet_pton(AF_INET, ip.c_str(), &serv_addr.sin_addr) <= 0) {
+        throw std::runtime_error("Invalid address");
+    }
+    if (bind(sockfd, (struct sockaddr*)&serv_addr, sizeof(serv_addr)) < 0) {
+        throw std::runtime_error("Failed to bind socket");
+    }
+}
+
+void ServerSocket::send(const std::string& message) {
+    if (sendto(sockfd, message.c_str(), message.size(), 0, (struct sockaddr*)&serv_addr, sizeof(serv_addr)) < 0) {
+        throw std::runtime_error("Failed to send message");
+    }
+}
+
+void ServerSocket::receive() {
+    char buffer[1024] = {0};
+    struct sockaddr_in cli_addr{};
+    socklen_t len = sizeof(cli_addr);
+    if (recvfrom(sockfd, buffer, 1024, 0, (struct sockaddr*)&cli_addr, &len) < 0) {
+        throw std::runtime_error("Failed to read from socket");
+    }
+    std::cout << "Received message from " << inet_ntoa(cli_addr.sin_addr) << ":" << ntohs(cli_addr.sin_port) << std::endl;
+    std::cout << "Message: " << buffer << std::endl;
+    lastMessage = buffer;
+    lastClientAddress = cli_addr;
+}
+
+void ServerSocket::run() {
+    while (true) {
+        receive();
+        if (lastMessage == "ping") {
+            send("pong");
+        }
+        if (lastMessage == "exit")
+            break;
+    }
+}
--- networking/server/ServerSocket.hpp
+++ networking/server/ServerSocket.hpp
@@ -6,23 +6,33 @@
 #define R_TYPE_SERVER_SERVERSOCKET_HPP
 
 #include <string>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <unistd.h>
+#include <string.h>
+#include <vector>
+#include <arpa/inet.h>
 #include "../shared/USocket.hpp"
-
 
 class ServerSocket : public USocket {
 public:
-    void connect(const std::string& ip, int port) override {
-        // Implémentation pour le serveur
-    }
+    ServerSocket();
+    ~ServerSocket();
 
-    void send(const std::string& message) override {
-        // Implémentation pour le serveur
-    }
+    void init_server(std::string ip, int port);
+    void send(const std::string& message) override;
+    void receive() override;
 
-    void receive() override {
-        // Implémentation pour le serveur
-    }
+    void run() override;
+
+private:
+    int sockfd;
+    struct sockaddr_in serv_addr;
+    std::string lastMessage;
+    // store clients
+    std::vector<struct sockaddr_in> clients;
+    struct sockaddr_in lastClientAddress;
+
 };
-
 
 #endif //R_TYPE_SERVER_SERVERSOCKET_HPP
