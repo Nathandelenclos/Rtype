4ceccdd3758bd797b0b8e0d894ff8b85c473b35c
TalleuxEwen <ewen.talleux@epitech.eu>
2023-12-28 23:44:38 +0100

    feat: adding Component interface class

---
+++ src/client/Components/IComponent.hpp
@@ -0,0 +1,34 @@
+//
+// Created by Talleux on 28/12/2023.
+//
+
+#pragma once
+
+#include <vector>
+#include <memory>
+
+typedef enum {
+    TEXT,
+    SPRITE,
+    BUTTON,
+    INPUT,
+    VARIABLE
+} ComponentType;
+
+class IComponent {
+    public:
+        virtual ~IComponent() = default;
+
+        [[nodiscard]] virtual ComponentType getType() const = 0;
+        virtual void action() = 0;
+        virtual void addActionTarget(std::unique_ptr<IComponent> component) = 0;
+        virtual void addSubComponent(std::unique_ptr<IComponent> component) = 0;
+
+    protected:
+        ComponentType _type;
+        std::vector<std::unique_ptr<IComponent>> action_target;
+        std::vector<std::unique_ptr<IComponent>> sub_components;
+};
+
+
+
