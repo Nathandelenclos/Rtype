aa73bb5827211562e524edeb7d7751ee425c00d9
TalleuxEwen <ewen.talleux@epitech.eu>
2023-12-28 10:47:14 +0100

    feat: init server networking in server main

--- src/server/main.cpp
+++ src/server/main.cpp
@@ -7,11 +7,16 @@
 
 #include <iostream>
 #include "../../include/DLLoader.hpp"
+#include "../../networking/server/ServerSocket.hpp"
 
 int main() {
     DLLoader loader("../lib_r_type.so");
 
     IGame *game = loader.getInstance("create");
+
+    ServerSocket serverSocket;
+
+    serverSocket.init_server("127.0.0.1", 6969);
 
     while (true) {
         for (auto service: game->getServices()) {
