a38204ce3bb22e23ab52b42ac43a855de98f405b
TalleuxEwen <ewen.talleux@epitech.eu>
2023-12-28 16:09:44 +0100

    del: remove useless print in update method of Graphic class

--- src/lib/r_type/services/Graphic.cpp
+++ src/lib/r_type/services/Graphic.cpp
@@ -9,7 +9,6 @@
 #include "Drawable.hpp"
 
 void Graphic::update(std::vector<IObject *> objects) {
-    std::cout << "Graphic::update()" << std::endl;
     for (auto &object : objects) {
         auto drawable = dynamic_cast<Drawable*>(object);
         if (drawable) {
