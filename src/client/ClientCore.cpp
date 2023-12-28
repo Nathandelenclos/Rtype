d4a9244466c50f27627a591f0c4ded47facbb514
TalleuxEwen <ewen.talleux@epitech.eu>
2023-12-28 22:28:50 +0100

    feat: adding ClientCore class

---
+++ src/client/ClientCore.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Talleux on 28/12/2023.
+//
+
+#include "ClientCore.hpp"
---
+++ src/client/ClientCore.hpp
@@ -0,0 +1,19 @@
+//
+// Created by Talleux on 28/12/2023.
+//
+
+#pragma once
+
+#include "../../networking/client/ClientSocket.hpp"
+
+class ClientCore {
+    public:
+        ClientCore() = default;
+        ~ClientCore() = default;
+        void run();
+
+        bool init_socket(std::string ip, int port);
+
+    private:
+        std::unique_ptr<ClientSocket> _socket;
+};
