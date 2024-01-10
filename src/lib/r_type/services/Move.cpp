ef2ddc2875c45dcd73d94f30aa3aac395a7c3019
Nather_ / Nathan Delenclos <nathan.delenclos@epitech.eu>
2024-01-10 15:46:42 +0100

    fix: deploy port

--- src/lib/r_type/services/Move.cpp
+++ src/lib/r_type/services/Move.cpp
@@ -13,7 +13,7 @@ void Move::update(std::shared_ptr<Event> event, std::shared_ptr<IComponentRType>
     if (drawable == nullptr || event == nullptr || attribute != "player " + std::to_string(event->id))
         return;
 
-    float speed = 10;
+    float speed = 5;
     auto [x, y] = drawable->getPosition();
     if (event->key == sf::Keyboard::Key::Up)
         y -= speed;
