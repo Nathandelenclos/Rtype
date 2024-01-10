d31f3443f5ac2bc38a9baf722d6a5510d03fef28
Nather_ / Nathan Delenclos <nathan.delenclos@epitech.eu>
2024-01-10 01:08:27 +0100

    feat: webhook teams

--- devops/roles/jenkins/files/jobs/main.groovy
+++ devops/roles/jenkins/files/jobs/main.groovy
@@ -1,21 +1,42 @@
 folder("Projects") {
-	description("The available projets")
+	description("The available projects")
 }
 
 freeStyleJob("link-project") {
-	description('Clé SSH publique:\n\n{{public_key}}')
+	description('SSH public key:\n\n{{public_key}}')
 	parameters {
 		stringParam("PROJECT_NAME", null, "Name of project")
 		stringParam("DISPLAY_NAME", null, "Display name for the job")
 		stringParam("GIT_URL", null, 'Git repository url')
 		stringParam("GIT_BRANCH", "master", "Git branch to use")
 		stringParam("REGISTRY_URL", "{{registry_domain}}", "Registry to store docker image")
+		stringParam("WEBHOOK_TEAMS", null, "Webhook url for teams")
 	}
 	steps {
 		shell("ssh-keyscan github.com >> /var/lib/jenkins/.ssh/known_hosts")
 		dsl {
 			text('''
 				freeStyleJob("Projects/$DISPLAY_NAME") {
+				    description("Project $PROJECT_NAME")
+				    properties {
+                        office365ConnectorWebhooks {
+                            webhooks {
+                                webhook {
+                                    name("$DISPLAY_NAME")
+                                    url("$WEBHOOK_TEAMS")
+                                    startNotification(false)
+                                    notifySuccess(true)
+                                    notifyAborted(false)
+                                    notifyNotBuilt(false)
+                                    notifyUnstable(true)
+                                    notifyFailure(true)
+                                    notifyBackToNormal(true)
+                                    notifyRepeatedFailure(false)
+                                    timeout(30000)
+                                }
+                            }
+                        }
+                    }
 					scm {
 						git {
 							remote {
